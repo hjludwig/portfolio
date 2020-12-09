c90c0ab75326b3ec56a77716bd83b11fc046a49b
Jeremy Ludwig <hjeremyludwig@gmail.com>
2020-12-09 09:59:39 -0500

    Added schemas

--- studio/schemas/project.js
+++ studio/schemas/project.js
@@ -9,9 +9,42 @@ export default {
             type: "string",
         },
         {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "name",
+                maxLength: 200,
+            },
+        },
+        {
             name: "client",
             title: "Client",
             type: "string",
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: "description",
+            title: "Description",
+            type: "text",
+        },
+        {
+            name: "tools",
+            title: "Tools",
+            type: "array",
+            of: [
+                {
+                    type: "reference",
+                    to: [{ type: "tool" }],
+                },
+            ],
         },
     ],
 };
---
+++ studio/schemas/tool.js
@@ -0,0 +1,12 @@
+export default {
+    name: "tool",
+    title: "Tools",
+    type: "document",
+    fields: [
+        {
+            name: "tool",
+            title: "Tool",
+            type: "string",
+        },
+    ],
+};
