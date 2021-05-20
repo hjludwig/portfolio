4a4433b4fd782cdc4899f7788bee8a54168aa6fe
Jeremy Ludwig <hjeremyludwig@gmail.com>
2021-05-20 13:18:59 -0400

    Add styled components plugin to gatsby-config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
@@ -40,8 +40,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
